17588333a82f2f9c54d58d89bdda2bc15d137b76
124c4a11 <124c4a11@mail.ru>
2019-05-01 14:14:17 +0300

    PostCard: base markup

---
+++ components/main/PostCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <el-card
+    class="post-card"
+    body-style="padding: 0"
+    shadow="hover"
+  >
+    <header class="post-card__header" slot="header">
+      <h3 class="post-card__title">Post title</h3>
+      <span>
+        <span class="el-icon-time"></span> {{ new Date().toLocaleString() }}
+      </span>
+    </header>
+
+    <div class="post-card__body">
+      <img class="post-card__img" src="https://cdn.tripzaza.com/ru/destinations/files/2017/09/Berlin-e1505798693967.jpg" alt="">
+    </div>
+
+    <footer class="post-card__footer">
+      <el-button round>Open</el-button>
+      <span><span class="el-icon-message"></span> 12</span>
+    </footer>
+  </el-card>
+</template>
+
+<style lang="scss" scoped>
+.post-card {
+  & + & { margin-top: 20px; }
+}
+
+.post-card__header,
+.post-card__footer {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.post-card__title {
+  margin: 0;
+  font-weight: 700;
+  font-size: 1.2rem;
+}
+
+.post-card__img {
+  display: block;
+  width: 100%;
+}
+
+.post-card__footer { padding: 20px; }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,26 @@
 <template>
-  <div></div>
+  <el-row type="flex" justify="center">
+    <el-col :sm="18" :md="12" :lg="10">
+      <app-post-card
+        v-for="post in 3"
+        :key="post"
+
+      />
+    </el-col>
+  </el-row>
 </template>
 
 
 <script>
+import AppPostCard from '@/components/main/PostCard.vue'
+
 export default {
+  head: {
+    title: 'Home'
+  },
+
+  components: {
+    AppPostCard
+  }
 }
 </script>
