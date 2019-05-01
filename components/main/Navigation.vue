510a4d7d8231d664cb7d6ffccdb866ec02d68b38
124c4a11 <124c4a11@mail.ru>
2019-05-01 13:10:05 +0300

    base layout

---
+++ components/main/Navigation.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav>
+    <a class="nav-logo" href="/">SSR Blog</a>
+  </nav>
+</template>
+
+
+<style lang="scss" scoped>
+  .nav-logo {
+    display: inline-block;
+    padding: 20px 0;
+    font-weight: 700;
+    font-size: 1.2rem;
+    color: #1f2f3d;
+    text-decoration: none;
+  }
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,27 @@
 <template>
-  <div>
-    <nuxt />
-  </div>
+  <el-container>
+    <el-header>
+      <app-navigation />
+    </el-header>
+    <el-main>
+      <nuxt />
+    </el-main>
+  </el-container>
 </template>
+
+<script>
+import AppNavigation from '@/components/main/Navigation.vue'
+
+export default {
+  components: {
+    AppNavigation
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.el-header {
+  border-bottom: 1px solid #e6e6e6;
+  text-align: center;
+}
+</style>
