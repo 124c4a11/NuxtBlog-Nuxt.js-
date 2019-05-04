f3f32934c5847a36b42d4d3ab93eb28fe7888511
124c4a11 <124c4a11@mail.ru>
2019-05-04 13:17:04 +0300

    Admin: base layout

---
+++ layouts/admin.vue
@@ -0,0 +1,12 @@
+<template>
+  <el-container>
+    <el-aside width="200px">Aside</el-aside>
+    <el-main>
+      <nuxt />
+    </el-main>
+  </el-container>
+</template>
+
+<style lang="scss" scoped>
+.el-container { height: 100vh; }
+</style>
