a95223834a20b1728e984974703d9072bb006bd2
124c4a11 <124c4a11@mail.ru>
2019-05-06 20:20:05 +0300

    Server create user: base logic

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -4,6 +4,7 @@ const keys = require('../keys')
 
 const User = require('../models/user.model')
 
+
 module.exports.login = async (req, res) => {
   const user = await User.findOne({ login: req.body.login })
 
@@ -25,6 +26,22 @@ module.exports.login = async (req, res) => {
   }
 }
 
-module.exports.createUser = (req, res) => {
 
+module.exports.createUser = async (req, res) => {
+  const candidate = await User.findOne({ login: req.body.login })
+
+  if (candidate) {
+    res.status(409).json({ message: 'This login already exists!' })
+  } else {
+    const salt = bcrypt.genSaltSync(10)
+
+    const user = new User({
+      login: req.body.login,
+      password: bcrypt.hashSync(req.body.password, salt)
+    })
+
+    await user.save()
+
+    res.status(201).json(user)
+  }
 }
