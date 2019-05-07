7d43cba534ddb740b2466c65cff621eb3cfedfa0
124c4a11 <124c4a11@mail.ru>
2019-05-07 14:38:33 +0300

    Server: passport

--- server/app.js
+++ server/app.js
@@ -1,19 +1,29 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const passport = require('passport')
+const passportStrategy = require('./middleware/passport.strategy')
 const keys = require('./keys')
 
 const authRoutes = require('./routes/auth.routes')
 
 const app = express()
 
+
 mongoose.connect(keys.MONGO_URI)
   .then(() => console.log('MongoDB connected...'))
   .catch((err) => console.error(err))
 
+
+app.use(passport.initialize())
+passport.use(passportStrategy)
+
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+
 app.use('/api/auth', authRoutes)
+
 
 module.exports = app
---
+++ server/middleware/passport.strategy.js
@@ -0,0 +1,26 @@
+const { Strategy, ExtractJwt } = require('passport-jwt')
+const { model } = require('mongoose')
+const keys = require('../keys')
+
+const User = model('users')
+
+const options = {
+  // Auterization: Bearer <token>
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: keys.JWT
+}
+
+
+module.exports = new Strategy(options, async (payload, done) => {
+  try {
+    const user = await User.findById(payload.userId).select('id')
+
+    if (user) {
+      done(null, user)
+    } else {
+      done(null, false)
+    }
+  } catch (err) {
+    console.error(e)
+  }
+})
