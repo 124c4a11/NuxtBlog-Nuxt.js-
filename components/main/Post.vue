5e3dabe19fb65f1fd3be6a8755a662ff2eab1005
124c4a11 <124c4a11@mail.ru>
2019-05-11 16:45:07 +0300

    .post__text p styles

--- components/main/Post.vue
+++ components/main/Post.vue
@@ -18,7 +18,7 @@
 
       <img class="post__img" :src="post.imageUrl" :alt="post.title">
 
-      <vue-markdown>{{post.text}}</vue-markdown>
+      <vue-markdown class="post__text">{{post.text}}</vue-markdown>
    </header>
   </article>
 </template>
@@ -33,45 +33,45 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-.post {
-  margin-bottom: 40px;
+<style lang="scss">
+  .post { margin-bottom: 40px; }
 
-  p {
-    font-size: 1.1rem;
-    line-height: 1.42;
+  .post__header-row {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
   }
-}
 
-.post__header-row {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.post__title {
-  margin-top: 0;
-  margin-bottom: 20px;
-  font-weight: 700;
-  font-size: 1.5rem;
-}
-
-.post__back-link {
-  font-size: 1.5rem;
-  color: #303133;
-  transition: color .25s;
-
-  &:hover,
-  &:focus {
-    outline: none;
-    color: #3a8ee6;
+  .post__title {
+    margin-top: 0;
+    margin-bottom: 20px;
+    font-weight: 700;
+    font-size: 1.5rem;
   }
-}
 
-.post__img {
-  display: block;
-  width: 100%;
-  height: auto;
-  margin: 20px 0;
-}
+  .post__back-link {
+    font-size: 1.5rem;
+    color: #303133;
+    transition: color .25s;
+
+    &:hover,
+    &:focus {
+      outline: none;
+      color: #3a8ee6;
+    }
+  }
+
+  .post__img {
+    display: block;
+    width: 100%;
+    height: auto;
+    margin: 20px 0;
+  }
+
+  .post__text {
+    p {
+      font-size: 1.1rem;
+      line-height: 1.5;
+    }
+  }
 </style>
